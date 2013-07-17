0bf76c4b36a6d47ebd03df94a3be511bbd2e869a
loganek <marcin.kolny@gmail.com>
2013-07-17 19:18:30 +0200

    dodalem jezyk angielski

---
+++ lang/en.php
@@ -0,0 +1,38 @@
+<?php
+$lang = array(
+	      "page_project" => "projekt strony",
+	      "official_website" => "officiaj website",
+	      "students_r&d" => "MIĘDZYWYDZIAŁOWE KOŁO NAUKOWE",
+	      "unmanned_flyers" => "BEZZAŁOGOWYCH OBIEKTÓW LATAJĄCYCH",
+	      "logout" => "Log out",
+	      "hello" => "Hello",
+	      "our_university" => "Silesian University of Technology",
+	      "search" => "Search",
+	      "news_arch" => "News archive",
+	      "hello_here" => "Witaj na oficjalnej stronie High Flyers",
+	      "hello_message" => "Angle of attack, glidescope, vertical stabilizers, Electronic Speed Controller, <b>programming until the wee hours of the morning</b>. If any of these phrases made shivers run down your spine or you just simply like electronics, informatics or model making you are in the right place! We proudly present to you the website of HIGH FLYERS – the Unmanned Aerial Vehicles students association.",
+	      "you_re_logged_in" => "Hey, you're already logged in!",
+	      "login" => "Nick",
+	      "password" => "Password",
+	      "log_in" => "login",
+	      "log_mod" => "Log In",
+	      "next" => "Next",
+	      "prev" => "Previous",
+	      "total_count_p" => "wszystkich stron",
+	      "partners" => "PARTNERS",
+	      "online" => "ONLINE",
+	      "info" => "INFORMATIONS",
+	      "more" => "more",
+	      "name" => "name",
+	      "second_name" => "second name",
+	      "and" => "and",
+	      "email" => "e-mail",
+	      "slave_duration" => "w kole od",
+	      "position" => "position",
+	      "must_be_logged_alert" => "Musisz być zalogowany aby móc oglądać tę stronę.",
+	      "main_page" => "Start",
+	      "news" => "News",
+	      "read_more" => "read more"
+);
+
+?>
