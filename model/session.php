57ca9b0696b7d2d4258f06462f51969cc6d1a6e4
loganek <marcin.kolny@gmail.com>
2013-07-17 21:19:23 +0200

    kompatybilnosc z php5.3

--- model/session.php
+++ model/session.php
@@ -1,56 +1,38 @@
 <?php
-
 require_once "common.php";
-
-class Session
-{	
-  function SessionStarted()
-  {
-    return ( session_status() == PHP_SESSION_ACTIVE );			
-  }
-  function StartSession()
-  {
-    if ( !$this->SessionStarted() )
-      session_start();
-  }
-	
-  function RegisterVar($varName, $varValue)
-  {
-    if ( $this->SessionStarted() )
-      $_SESSION[$varName] = $varValue;
-    else
-      {
-	ShowError("Sesja nie zostala rozpoczeta.");
-	return ErrorLevel::ERROR;
-      }
-  }
-	
-  function GetSessionVar($varName)
-  {
-    if ( $this->SessionStarted() )
-      {
-	if ( isset( $_SESSION[$varName] ) )
-	  {
-	    return $_SESSION[$varName];
-	  }
-	else 
-	  {
-	    return ErrorLevel::WARNING;
-	  }
-      }
-    else
-      {
-	ShowError("Sesja nie zostala rozpoczeta.");
-	return ErrorLevel::ERROR; 
-      }
-  }
-  function Clear()
-  {
-  	$language = $_SESSION['hf_lang'];
-    $_SESSION = array();
-    $_SESSION['hf_lang'] = $language;
-  }
-	
+class Session {
+	function SessionStarted() {
+		return false; // return ( session_status() == PHP_SESSION_ACTIVE );
+	}
+	function StartSession() {
+		if (! $this->SessionStarted ())
+			session_start ();
+	}
+	function RegisterVar($varName, $varValue) {
+		if ($this->SessionStarted ())
+			$_SESSION [$varName] = $varValue;
+		else {
+			ShowError ( "Sesja nie zostala rozpoczeta." );
+			return ErrorLevel::ERROR;
+		}
+	}
+	function GetSessionVar($varName) {
+		if ($this->SessionStarted ()) {
+			if (isset ( $_SESSION [$varName] )) {
+				return $_SESSION [$varName];
+			} else {
+				return ErrorLevel::WARNING;
+			}
+		} else {
+			ShowError ( "Sesja nie zostala rozpoczeta." );
+			return ErrorLevel::ERROR;
+		}
+	}
+	function Clear() {
+		$language = $_SESSION ['hf_lang'];
+		$_SESSION = array ();
+		$_SESSION ['hf_lang'] = $language;
+	}
 }
 
 ?>
