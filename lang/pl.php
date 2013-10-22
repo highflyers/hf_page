57cde2acfdaf69a5ebda7bbc7157fda672631133
Marcin Kolny <marcin.kolny@gmail.com>
2013-10-23 00:20:06 +0200

    updated "hello" text

--- lang/pl.php
+++ lang/pl.php
@@ -10,7 +10,9 @@ $lang = array(
 	      "search" => "Szukaj",
 	      "news_arch" => "Archiwum aktualności",
 	      "hello_here" => "Witaj na oficjalnej stronie High Flyers",
-	      "hello_message" => "Kąt natarcia skrzydeł. Algorytm regulacji. Biegunowa prędkości. Lot autonomiczny. <b>Programowanie do późnych godzin nocnych</b>. Jeśli któreś z tych wyrażeń wywołało u Ciebie dreszcz ekscytacji lub zwyczajnie interesujesz się elektroniką, informatyką czy też modelarstwem, świetnie trafiłeś! Mamy zaszczyt powitać Cię na stronie Międzywydziałowego Koła Naukowego Bezzałogowych Obiektów Latających High Flyers.",
+	      "hello_message" => "Rok 2010. Listopadowy wieczór. Laboratorium 935 na Wydziale Automatyki, Elektroniki i Informatyki.
+Pośród szelestu szkiców rodem z pracowni Leonarda da Vinci, w migotliwym świetle projektora zgromadziła się grupka młodych ludzi. Kąt natarcia skrzydeł. Algorytmy regulacji. Loty zwiadowcze. Autonomia. Kod źródłowy. Wideo rejestracja z lotu ptaka. Systemy embedded. Z użyciem tego typu określeń porozumiewali się między sobą. W takich właśnie warunkach narodziło się Międzywydziałowe Koło Naukowe Bezzałogowych Obiektów Latających High Flyers.
+Dziś jesteśmy organizacją zrzeszającą 60 studentów z 3 Wydziałów Politechniki Śląskiej. Nasze główne cele to poszerzanie specjalistycznej wiedzy w zakresie projektowania, budowy i eksploatacji autonomicznie sterowanych obiektów latających typu UAV (ang. Unmanned Aerial Vehicle), udział w zawodach międzynarodowych oraz współpraca z firmami różnych branż poczynając od biur projektowych, dystrybutorów sprzętu i oprogramowania po producentów i dostawców usług. Jeśli więc interesujesz się elektroniką, mechaniką, informatyką, modelarstwem, analizą obrazów, zarządzaniem lub public relations w organizacji to świetnie trafiłeś/aś!",
 	      "you_re_logged_in" => "Hej, ty juz jestes przeciez zalogowany",
 	      "login" => "Login",
 	      "password" => "Hasło",
