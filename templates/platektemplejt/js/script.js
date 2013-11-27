89f065e55a6639c829447a0742e6344e572b05b9
Michal Platek <michal.platek.hf@gmail.com>
2013-11-27 18:38:22 +0100

    One more detail changed in order to have the flags working

--- templates/platektemplejt/js/script.js
+++ templates/platektemplejt/js/script.js
@@ -1,5 +1,5 @@
 var $buoop = {vs:{i:7,f:4,o:10.5,s:4,n:9}};
-var tplUrl = "/templates/platektemplejt/"; 
+var tplUrl = "templates/platektemplejt/"; 
 $buoop.ol = window.onload; 
 window.onload = function() { 
 	try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
@@ -10,6 +10,7 @@ window.onload = function() {
 };
 
 $(document).ready(function() {
+	
 	ddsmoothmenu.init({
 		mainmenuid: "mainMenu", //menu DIV id
 		orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
@@ -28,10 +29,25 @@ $(document).ready(function() {
 	$(".socialButton img").hover(
     function(){
         var id = $(this).parent().get(0).getAttribute("id");
-	    $(this).attr("src", tplUrl + "/gfx/" + id + "_over.png");
+	    $(this).attr("src", tplUrl + "gfx/" + id + "_over.png");
 	}, 
 	function() {
 	    var id = $(this).parent().get(0).getAttribute("id");
-	    $(this).attr("src", tplUrl + "/gfx/" + id + ".png");
+	    $(this).attr("src", tplUrl + "gfx/" + id + ".png");
+	});
+	
+	$("#flagEN").hover(
+    function(){
+        $(this).attr("src", tplUrl + "gfx/icons/flagEN_1.png");
+	}, 
+	function() {
+	    $(this).attr("src", tplUrl + "gfx/icons/flagEN_0.png");
+	});
+	$("#flagPL").hover(
+    function(){
+        $(this).attr("src", tplUrl + "gfx/icons/flagPL_1.png");
+	}, 
+	function() {
+	    $(this).attr("src", tplUrl + "gfx/icons/flagPL_0.png");
 	});
 });
